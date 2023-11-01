1c21de48c13f6f846b5d4a2394ad9c8deccf9868
钱格 <webqiange@163.com>
2023-11-01 22:41:12 +0800

    -m

--- test01/1.js
+++ test01/1.js
@@ -1,1 +1,31 @@
-console.log(123)
+const EventEmitter = require('events')
+
+const myEvent = new EventEmitter()
+
+// 异步调用监听器
+// 监听器函数可以使用 setImmediate() 或 process.nextTick() 方法切换到异步操作模式
+myEvent.on('eventName2', (arg1, arg2) => {
+	setImmediate(() => {
+		console.log('异步执行：1、触发eventName2事件', arg1, arg2)
+	})
+})
+
+myEvent.emit('eventName2', 'abc', '123')
+
+// 使用方式
+myEvent.on('eventName', () => {
+	console.log('1、触发eventName事件')
+})
+
+myEvent.on('eventName', () => {
+	console.log('2、触发eventName事件')
+})
+
+myEvent.emit('eventName')
+
+// 添加参数
+myEvent.on('eventName1', (arg1, arg2) => {
+	console.log('1、触发eventName1事件', arg1, arg2)
+})
+
+myEvent.emit('eventName1', 'abc', '123')
---
+++ test01/2.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+// CPU密集型操作并不适合在node中处理，如下
+const sleepTime = (time) => {
+	const sleep = Date.now() + time * 1000
+	while (Date.now() < sleep) {
+		console.log('sleeping...')
+	}
+	return
+}
+
+sleepTime(6)
+
+const server = http.createServer((req, res) => {
+	res.end('hello world......')
+})
+
+server.listen(8080, () => {
+	console.log('启动服务......')
+})
